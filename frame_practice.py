4ccde89e8d1f6c4832a904191117cf4940145c62
Ariadne Rincon <54596709+arincon10912@users.noreply.github.com>
2022-09-15 14:47:18 -0700

    Got the format for the first two frames,

--- frame_practice.py
+++ frame_practice.py
@@ -1,8 +1,10 @@
-#project start
-#started practicing on using multiple frames as this will be base of project
+# Ariadne Rincon
+# File where I am practicing how to make and customize frames with tkinter
+# PROJECT START
 import tkinter as tk                
 from tkinter import font as tkfont
 
+#Shared details for all classes
 class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
@@ -30,7 +32,7 @@ class SampleApp(tk.Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
-
+#Frame with checkboxes
 class AllergyPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -40,6 +42,7 @@ class AllergyPage(tk.Frame):
         label = tk.Label(self, text="Check all your known allergies:", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
         
+        #s = tk.IntVar() this may work to save variable?
         soy = tk.Checkbutton(self, text="soy")
         wheat = tk.Checkbutton(self, text="wheat")
         nuts = tk.Checkbutton(self, text="nuts")
@@ -54,13 +57,11 @@ class AllergyPage(tk.Frame):
 
         button1 = tk.Button(self, text="Confirm Choices",
                             command=lambda: controller.show_frame("FoodSelectionPage"))
-        #button2 = tk.Button(self, text="Go to OptionsPage",
-                            #command=lambda: controller.show_frame("OptionsPage"))
         button2 = tk.Button(self, text="Quit",command = quit)
         button1.pack()
         button2.pack()
 
-
+#Frame with Food Options
 class FoodSelectionPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -68,13 +69,19 @@ class FoodSelectionPage(tk.Frame):
         self.controller = controller
         label = tk.Label(self, text="Choose a Food Option: ", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
+
+        canes = tk.Radiobutton(self, text = "Canes", value = 1)
+        panda = tk.Radiobutton(self, text = "Panda Express", value = 2)
+        canes.pack()
+        panda.pack()
+
         button1 = tk.Button(self, text="Next",
                            command=lambda: controller.show_frame("OptionsPage"))
         button2 = tk.Button(self, text="Quit",command = quit)
         button1.pack()
         button2.pack()
 
-
+#Frame with Safe Food Options
 class OptionsPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -88,7 +95,7 @@ class OptionsPage(tk.Frame):
         button1.pack()
         button2.pack()
 
-
+#Keep Program Running 
 if __name__ == "__main__":
     app = SampleApp()
     app.mainloop()
